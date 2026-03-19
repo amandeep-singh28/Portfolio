5282599c324f604a10d2f4dd97a8bcbd7195e7b8
Amandeep Singh <amandeep87557@gmail.com>
2026-03-19 23:06:40 +0530

    Updation settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,10 +52,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 
 # ✅ STATIC FILES (IMPORTANT FIX)
