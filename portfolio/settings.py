3b5055ce9987585e514cde12fab399f846f89e54
Amandeep Singh <amandeep87557@gmail.com>
2026-03-20 14:11:07 +0530

    Changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -98,5 +98,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('amandeepsingh892333@gmail.com', '')
-EMAIL_HOST_PASSWORD = os.environ.get('unae wcqt jjyb eibn', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
